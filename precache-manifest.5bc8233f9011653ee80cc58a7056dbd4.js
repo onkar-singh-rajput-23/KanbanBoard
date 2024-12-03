81e4ef0916780fba11f52ad1220363db3fc78331
onkar <singhonkar2020@gmail.com>
2024-12-03 11:54:51 +0530

    Updates

---
+++ precache-manifest.5bc8233f9011653ee80cc58a7056dbd4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14911fb30cf5ad25ea882471543d4fbe",
+    "url": "/KanbanBoard/index.html"
+  },
+  {
+    "revision": "a35f0e01347c18c44628",
+    "url": "/KanbanBoard/static/css/main.0f819f07.chunk.css"
+  },
+  {
+    "revision": "7e1d2b5b61a5e126f38c",
+    "url": "/KanbanBoard/static/js/2.53faad34.chunk.js"
+  },
+  {
+    "revision": "1876019e6f8cf7ca83e2",
+    "url": "/KanbanBoard/static/js/3.0e4cdb98.chunk.js"
+  },
+  {
+    "revision": "a35f0e01347c18c44628",
+    "url": "/KanbanBoard/static/js/main.7ee7403f.chunk.js"
+  },
+  {
+    "revision": "3b1f11105ef29e317395",
+    "url": "/KanbanBoard/static/js/runtime~main.dfed13b4.js"
+  },
+  {
+    "revision": "47973b095119ca88f2cf7435ae64b951",
+    "url": "/KanbanBoard/static/media/Backlog.47973b09.svg"
+  },
+  {
+    "revision": "15c84b141088b15567be2fc776da7576",
+    "url": "/KanbanBoard/static/media/Cancelled.15c84b14.svg"
+  },
+  {
+    "revision": "b96d92a33a5b1ec307931b7e2a8d7d32",
+    "url": "/KanbanBoard/static/media/Display.b96d92a3.svg"
+  },
+  {
+    "revision": "ee99e204ff8124cf3efad786a4035a57",
+    "url": "/KanbanBoard/static/media/Done.ee99e204.svg"
+  },
+  {
+    "revision": "0d2cbadcaecb54771adf2fc3a4b7e6db",
+    "url": "/KanbanBoard/static/media/No-priority.0d2cbadc.svg"
+  },
+  {
+    "revision": "991aa8ffc2f2062e7a647ea42988b361",
+    "url": "/KanbanBoard/static/media/To-do.991aa8ff.svg"
+  },
+  {
+    "revision": "a19048c91b13f82bfcbc6917f2586ec9",
+    "url": "/KanbanBoard/static/media/add.a19048c9.svg"
+  },
+  {
+    "revision": "6c272d9dbc2afbd2bcb03b63c12fec74",
+    "url": "/KanbanBoard/static/media/dot.6c272d9d.svg"
+  },
+  {
+    "revision": "990a90264a79616a3efb8d5b525d4d66",
+    "url": "/KanbanBoard/static/media/down.990a9026.svg"
+  },
+  {
+    "revision": "b773d8483288a2960bd952df15e1d28f",
+    "url": "/KanbanBoard/static/media/feature.b773d848.png"
+  },
+  {
+    "revision": "10cfcedf7c5bca2c132f8c693ea09b8d",
+    "url": "/KanbanBoard/static/media/high.10cfcedf.svg"
+  },
+  {
+    "revision": "6f7f6c8e224d8356b240a937ce34069f",
+    "url": "/KanbanBoard/static/media/in-progress.6f7f6c8e.svg"
+  },
+  {
+    "revision": "2ad281d36a00948dc82df990fa6ed849",
+    "url": "/KanbanBoard/static/media/low.2ad281d3.svg"
+  },
+  {
+    "revision": "861ebea52035117975c064836e6b5f25",
+    "url": "/KanbanBoard/static/media/mediam.861ebea5.svg"
+  },
+  {
+    "revision": "4eca8404cb3c7d84e8185cf924ec035f",
+    "url": "/KanbanBoard/static/media/urgent.4eca8404.svg"
+  }
+]);
